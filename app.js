fdd973573e79b8f05bf9fcb3daf0f0a46b3b97d0
rajatpanchal980 <panchalrajat774@gmail.com>
2024-12-07 12:42:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
 // Create a WebSocket connection
-const ws = new WebSocket('ws://chatapp1-8hkv.onrender.com')
+const ws = new WebSocket('https://chatapp1-8hkv.onrender.com')
 
 ws.onmessage = (event) => {
   console.log('Message received from server')
